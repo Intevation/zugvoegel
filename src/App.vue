90448dd060fde7714c01e1499c107edff039e3d3
Bjoern Schilberg <bjoern@intevation.de>
2019-06-14 22:10:31 +0200

    Formatting

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,41 @@
 <template>
   <v-app id="inspire" light>
-    <v-navigation-drawer v-model="drawer" disable-resize-watcher clipped app right mobile-break-point=0>
-       <v-list>
-          <v-list-group
-            v-for="item in items"
-            :key="item.title"
-            v-model="item.active"
-            :prepend-icon="item.action"
-            no-action
-          >
-            <template v-slot:activator>
-              <v-list-tile>
-                <v-list-tile-content>
-                  <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-                </v-list-tile-content>
-              </v-list-tile>
-            </template>
-
-            <v-list-tile
-              v-for="subItem in item.items"
-              :key="subItem.title"
-              @click=""
-            >
+    <v-navigation-drawer
+      v-model="drawer"
+      disable-resize-watcher
+      clipped
+      app
+      right
+      mobile-break-point="0"
+    >
+      <v-list>
+        <v-list-group
+          v-for="item in items"
+          :key="item.title"
+          v-model="item.active"
+          :prepend-icon="item.action"
+          no-action
+        >
+          <template v-slot:activator>
+            <v-list-tile>
               <v-list-tile-content>
-                <v-list-tile-title>{{ subItem.title }}</v-list-tile-title>
+                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
               </v-list-tile-content>
-
-              <v-list-tile-action>
-                <v-checkbox
-                  v-model=subItem.active
-                ></v-checkbox>
-                <v-icon>{{ subItem.action }}</v-icon>
-              </v-list-tile-action>
             </v-list-tile>
-          </v-list-group>
-        </v-list>
+          </template>
+
+          <v-list-tile v-for="subItem in item.items" :key="subItem.title" @click>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ subItem.title }}</v-list-tile-title>
+            </v-list-tile-content>
+
+            <v-list-tile-action>
+              <v-checkbox v-model="subItem.active"></v-checkbox>
+              <v-icon>{{ subItem.action }}</v-icon>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list-group>
+      </v-list>
     </v-navigation-drawer>
     <v-toolbar app absolute clipped-right color="#0068b4" dark>
       <v-toolbar-title right>Zugvögel auf Reisen</v-toolbar-title>
@@ -59,27 +60,27 @@ export default {
   data: () => ({
     drawer: null,
     items: [
-        {
-          action: 'gps_fixed',
-          title: 'Reise 2019 / 2020',
-          active: true,
-          items: [
-            { title: 'Adele' , active:true},
-            { title: 'Joris' , active:true},
-            { title: 'Teelke', active:true}
-          ]
-        },
-        {
-          action: 'gps_fixed',
-          title: 'Reise 2018 / 2019',
-          active: false,
-          items: [
-            { title: 'Adele' , active:false},
-            { title: 'Joris' , active:false},
-            { title: 'Teelke', active:false}
-          ]
-        }
-      ]
+      {
+        action: "gps_fixed",
+        title: "Reise 2019 / 2020",
+        active: true,
+        items: [
+          { title: "Adele", active: true },
+          { title: "Joris", active: true },
+          { title: "Teelke", active: true }
+        ]
+      },
+      {
+        action: "gps_fixed",
+        title: "Reise 2018 / 2019",
+        active: false,
+        items: [
+          { title: "Adele", active: false },
+          { title: "Joris", active: false },
+          { title: "Teelke", active: false }
+        ]
+      }
+    ]
   }),
   props: {
     source: String
