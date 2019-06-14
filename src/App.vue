34955001ee003bb18a416a980526451a5f3904f0
Bjoern Schilberg <bjoern@intevation.de>
2019-06-14 21:03:23 +0200

    First vuetify leaflet prototyp

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire" light>
+    <v-navigation-drawer v-model="drawer" clipped fixed app right>
+      <v-list dense>
+        <v-list-tile v-on:click>
+          <v-list-tile-action>
+            <v-icon>dashboard</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Dashboard</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile v-on:click>
+          <v-list-tile-action>
+            <v-icon>settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Settings</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app absolute clipped-right color="purple">
+      <v-spacer></v-spacer>
+      <v-toolbar-title right>Turteltauben</v-toolbar-title>
+      <v-toolbar-side-icon v-on:click.stop="drawer = !drawer"></v-toolbar-side-icon>
+    </v-toolbar>
+    <v-content>
+      <Map></Map>
+    </v-content>
+    <!--v-footer app fixed>
+      <span>&copy; 2019</span>
+    </v-footer-->
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Map from "./components/Map";
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "App",
+  components: { Map },
+  data: () => ({
+    drawer: null
+  }),
+  props: {
+    source: String
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Map.vue
@@ -0,0 +1,51 @@
+<template>
+    <div id="map"></div>
+</template>
+
+<script>
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+export default {
+  data: () => ({
+    map: {}
+  }),
+  mounted() {
+
+    let map = L.map("map", {
+      attributionControl: false,
+      center: [50.15, 10.66],
+      zoom: 5,
+      maxZoom: 18,
+      minZoom: 5,
+      maxBounds: [[42, -46], [58, 67]],
+      fadeAnimation: false,
+      zoomControl: false
+      // renderer: L.canvas()
+    });
+
+    L.tileLayer(
+      "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
+      {
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: "mapbox.streets",
+        accessToken:
+          "pk.eyJ1IjoiYmpvZXJuc2NoaWxiZXJnIiwiYSI6InRzOVZKeWsifQ.y20mr9o3MolFOUdTQekhUA",
+        noWrap: true
+      }
+    ).addTo(map);
+
+    this.map=map;
+
+  }
+};
+</script>
+
+<style>
+#map {
+    height: 100%;
+    width: 100%;
+}
+</style>
