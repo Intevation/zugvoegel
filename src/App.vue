857823ebe298786870fda5ad7fd30d351e28541c
Bjoern Schilberg <bjoern@intevation.de>
2019-06-23 14:00:21 +0200

    First translated version.

--- src/App.vue
+++ src/App.vue
@@ -8,45 +8,65 @@
       right
       mobile-break-point="0"
     >
-      <v-list>
-        <v-subheader>Flugrouten</v-subheader>
+      <v-list v-model="phrases">
+        <v-subheader>{{phrases.routes}}</v-subheader>
         <v-list-group
-          v-for="item in items"
-          :key="item.title"
-          v-model="item.active"
-          :prepend-icon="item.action"
+          v-for="season in seasons"
+          :key="season.title"
+          v-model="season.active"
+          :prepend-icon="season.action"
           no-action
         >
           <template v-slot:activator>
             <v-list-tile>
               <v-list-tile-content>
-                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                <v-list-tile-title>{{phrases.journey}} {{ season.title }}</v-list-tile-title>
               </v-list-tile-content>
             </v-list-tile>
           </template>
 
-          <v-list-tile v-for="subItem in item.items" :key="subItem.title" @click>
+          <v-list-tile v-for="turtledove in season.turtledoves" :key="turtledove.name">
             <v-list-tile-content>
-              <v-list-tile-title>{{ subItem.title }}</v-list-tile-title>
+              <v-list-tile-title>{{ turtledove.name}}</v-list-tile-title>
             </v-list-tile-content>
 
             <v-list-tile-action>
-              <v-checkbox v-model="subItem.active"></v-checkbox>
-              <v-icon>{{ subItem.action }}</v-icon>
+              <v-checkbox v-model="turtledove.active"></v-checkbox>
             </v-list-tile-action>
           </v-list-tile>
         </v-list-group>
-        <v-subheader>Wer ist wer?</v-subheader>
+        <v-subheader>{{phrases.who}}</v-subheader>
+        <v-list-group>
+          <template v-slot:activator>
+            <v-list-tile>
+              <v-list-tile-content>
+                <v-list-tile-title>{{phrases.language}}</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </template>
+              <v-radio-group column>
+          <v-list-tile>
+            <v-list-tile-action @click="phrases=phrasesEN">
+                <v-radio :label=phrases.en value="radio-en" ></v-radio>
+            </v-list-tile-action>
+          </v-list-tile>
+          <v-list-tile>
+            <v-list-tile-action @click="phrases=phrasesDE">
+                <v-radio :label=phrases.de value="radio-de" ></v-radio>
+            </v-list-tile-action>
+          </v-list-tile>
+              </v-radio-group>
+        </v-list-group>
       </v-list>
     </v-navigation-drawer>
     <v-toolbar app absolute clipped-right color="#0068b4" dark>
-      <v-toolbar-title right>Zugvögel auf Reisen</v-toolbar-title>
+      <v-toolbar-title right>{{phrases.title}}</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-title right>Turteltauben</v-toolbar-title>
+      <v-toolbar-title right>{{phrases.turtledoves}}</v-toolbar-title>
       <v-toolbar-side-icon v-on:click.stop="drawer = !drawer"></v-toolbar-side-icon>
     </v-toolbar>
     <v-content>
-      <Map :items=items></Map>
+      <Map :seasons="seasons"></Map>
     </v-content>
     <!--v-footer app fixed>
       <span>&copy; 2019</span>
@@ -56,36 +76,90 @@
 
 <script>
 import Map from "./components/Map";
+import Polyglot from "node-polyglot";
+
 export default {
   name: "App",
   components: { Map },
   data: () => ({
     drawer: null,
-    items: [
+    radios: "radio-en",
+    locale: "",
+    language: "",
+    phrasesDE: {
+      hello: "Hallo",
+      map: "Karte",
+      title: "Zugvögel auf Reisen",
+      turtledoves: "Turteltauben",
+      routes: "Flugrouten",
+      journey: "Reise",
+      who: "Wer ist wer?",
+      language: "Sprache auswählen",
+      en: "Englisch",
+      de: "Deutsch",
+      satellite: "Satellit",
+      flightRoute: "Flugstrecke seit letzter Ortung:",
+      nicolaFail: "Sender ist im August 2017 ausgefallen.",
+      janFail: "Sender ist im September 2017 ausgefallen."
+    },
+    phrasesEN: {
+      hello: "Hello",
+      title: "Migratory birds travelling",
+      turtledoves: "Turteldoves",
+      routes: "Flight routes",
+      journey: "Journey",
+      who: "Who is who?",
+      language: "Select langugage",
+      en: "English",
+      de: "German",
+      map: "Map",
+      satellite: "Satellite",
+      flightRoute: "Distance from last location:",
+      nicolaFail: "Transmitter was not operational anymore since August 2017.",
+      janFail: "Transmitter was not operational anymore since September 2017."
+    },
+    phrases: {},
+    polyglot: {},
+    seasons: [
       {
         action: "gps_fixed",
-        title: "Reise 2019 / 2020",
-        active: true,
-        items: [
-          { title: "Adele", active: true },
-          { title: "Joris", active: true },
-          { title: "Teelke", active: true }
+        title: "2018 / 2019",
+        active: false,
+        turtledoves: [{ name: "Francesco", active: false }]
+      },
+      {
+        action: "gps_fixed",
+        title: "2017 / 2018",
+        active: false,
+        turtledoves: [
+          { name: "Dana", active: false },
+          { name: "Francesco", active: false },
+          { name: "Nicola", active: false }
         ]
       },
       {
         action: "gps_fixed",
-        title: "Reise 2018 / 2019",
+        title: "2016 / 2017",
         active: false,
-        items: [
-          { title: "Adele", active: false },
-          { title: "Joris", active: false },
-          { title: "Teelke", active: false }
-        ]
+        turtledoves: [{ name: "Nicola", active: false }]
       }
     ]
   }),
-  props: {
-    source: String
+  created: function() {
+    //this.polyglot= new Polyglot();
+    this.language =
+      (navigator.languages && navigator.languages[0]) ||
+      navigator.language ||
+      navigator.userLanguage;
+    this.locale = this.language.substring(0, 2);
+    this.phrases = {
+      de: this.phrasesDE,
+      en: this.phrasesEN
+    }[this.locale === "de" ? "de" : "en"];
+    this.polyglot = new Polyglot({
+      locale: this.locale,
+      phrases: this.phrases
+    });
   }
 };
 </script>
