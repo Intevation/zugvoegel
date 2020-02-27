cb456f494e77671ad1443ebbb72675b2cc30add2
Bjoern Schilberg <bj03rn.5ch1lb3r6@posteo.de>
2020-02-27 22:04:09 +0100

    Splitted navi into seperate component

--- src/App.vue
+++ src/App.vue
@@ -7,120 +7,24 @@
       :mini-variant.sync="mini"
       @mouseover.native="mini=false"
       @mouseleave.native="mini=true">
-      <v-list v-model="phrases">
-        <v-list-group
-          value="true"
-          no-action>
-          <template v-slot:activator>
-            <v-list-item-icon>
-              <v-icon>mdi-crosshairs-gps</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>{{ phrases.routes }}</v-list-item-title>
-            </v-list-item-content>
-          </template>
-          <v-list-group
-            v-for="season in seasons"
-            :key="season.title"
-            v-model="season.active"
-            :prepend-icon="season.action"
-            no-action
-            sub-group>
-            <template v-slot:activator>
-              <v-list-item-title>{{ phrases.journey }} {{ season.title }}</v-list-item-title>
-            </template>
-            <v-list-item
-              v-for="turtledove in season.turtledoves"
-              :key="turtledove.name">
-              <v-list-item-content>
-                <v-list-item-title>{{ turtledove.name }}</v-list-item-title>
-              </v-list-item-content>
-              <v-list-item-action>
-                <v-switch
-                  v-model="turtledove.active"
-                  inset />
-              </v-list-item-action>
-            </v-list-item>
-          </v-list-group>
-        </v-list-group>
-
-        <v-list-group
-          prepend-icon="mdi-twitter-circle"
-          no-action>
-          <template v-slot:activator>
-            <v-list-item-title>{{ phrases.who }}</v-list-item-title>
-          </template>
-          <v-list-item
-            v-for="(td, index) in turtledoves"
-            :key="index"
-            v-model="turtledoves"
-            inactive
-            ripple
-            @click="openLink(td.blog)">
-            <v-divider v-if="td.divider" />
-            <v-list-item-avatar v-if="td.avatar">
-              <v-img :src="td.avatar" />
-            </v-list-item-avatar>
-            <v-list-item-content v-if="td.name">
-              <v-list-item-title v-text="td.name" />
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-group>
-
-        <v-list-group
-          prepend-icon="mdi-layers"
-          no-action>
-          <template v-slot:activator>
-            <v-list-item-title>{{ phrases.backgroundmap }}</v-list-item-title>
-          </template>
-          <v-list-item>
-            <v-radio-group
-              v-model="backgroundmap"
-              column>
-              <v-radio
-                :label="phrases.satellite"
-                value="satellite" />
-              <v-radio
-                :label="phrases.streetmap"
-                value="streetmap" />
-            </v-radio-group>
-          </v-list-item>
-        </v-list-group>
-
-        <v-list-group
-          prepend-icon="mdi-translate"
-          no-action>
-          <template v-slot:activator>
-            <v-list-item-title>{{ phrases.language }}</v-list-item-title>
-          </template>
-          <v-list-item>
-            <v-radio-group
-              v-model="radios"
-              column>
-              <v-radio
-                :label="phrases.en"
-                value="radio-en" />
-              <v-radio
-                :label="phrases.de"
-                value="radio-de" />
-            </v-radio-group>
-          </v-list-item>
-        </v-list-group>
-      </v-list>
+      <Navi
+        :phrases="phrases"
+        :seasons.sync="seasons"
+        :turtledoves.sync="turtledoves"
+        :backgroundmap.sync="backgroundmap"
+        :language.sync="language" />
     </v-navigation-drawer>
 
     <Menu :mini.sync="mini" />
+
     <v-content>
       <Map
-        :seasons="seasons"
-        :turtledoves="turtledoves"
+        :seasons.sync="seasons"
+        :turtledoves.sync="turtledoves"
         :phrases="phrases"
-        :backgroundmap="backgroundmap"
+        :backgroundmap.sync="backgroundmap"
         :mini.sync="mini" />
     </v-content>
-    <!--v-footer app fixed>
-      <span>&copy; 2019</span>
-    </v-footer-->
   </v-app>
 </template>
 
@@ -130,62 +34,44 @@ import turtledoves from "./config/turtledoves.json";
 import phrasesEN from "./locales/phrasesEN.json";
 import phrasesDE from "./locales/phrasesDE.json";
 import Map from "./components/Map";
+import Navi from "./components/Navi";
 import "@mdi/font/css/materialdesignicons.css";
 import Menu from "./components/Menu";
 
 export default {
   name: "App",
-  components: { Menu, Map },
+  components: { Menu, Map, Navi},
   data: () => ({
     mini: true,
-    locale: "",
-    language: "",
+    phrases: {},
     phrasesDE: phrasesDE,
     phrasesEN: phrasesEN,
-    phrases: {},
-    radios: "",
     backgroundmap: "streetmap",
     seasons: seasons,
-    turtledoves: turtledoves
+    turtledoves: turtledoves,
+    language: ""
   }),
   watch: {
     // function(newVal, oldVal)
-    radios: function(newVal) {
-      if (newVal === "radio-de") {
+    language: function(newVal) {
+      if (newVal === "de") {
         this.phrases = this.phrasesDE;
       } else {
         this.phrases = this.phrasesEN;
       }
     },
-    // function(newVal, oldVal)
-    phrases: function() {
-      //console.log("newVal:" + newVal.janFail, "oldVal:" + oldVal.janFail);
-      let jan = this.turtledoves.filter(function(td) {
-        return td.name == "Jan";
-      })[0];
-      jan.note = this.phrases.janFail;
-      let nicola = this.turtledoves.filter(function(td) {
-        return td.name == "Nicola";
-      })[0];
-      nicola.note = this.phrases.nicolaFail;
-    }
   },
   created: function() {
-    this.language =
+    let language =
       (navigator.languages && navigator.languages[0]) ||
       navigator.language ||
       navigator.userLanguage;
-    this.locale = this.language.substring(0, 2);
+      console.log(language);
+    this.language = language.substring(0, 2);
     this.phrases = {
       de: this.phrasesDE,
       en: this.phrasesEN
-    }[this.locale === "de" ? "de" : "en"];
-    this.radios = "radio-" + this.locale;
-  },
-  methods: {
-    openLink(link) {
-      window.open(link, "_blank");
-    }
+    }[language=== "de" ? "de" : "en"];
   }
 };
 </script>
---
+++ src/components/Navi.vue
@@ -0,0 +1,151 @@
+<template>
+  <v-list v-model="phrases">
+    <v-list-group
+      value="true"
+      no-action>
+      <template v-slot:activator>
+        <v-list-item-icon>
+          <v-icon>mdi-crosshairs-gps</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>{{ phrases.routes }}</v-list-item-title>
+        </v-list-item-content>
+      </template>
+      <v-list-group
+        v-for="season in seasons"
+        :key="season.title"
+        v-model="season.active"
+        :prepend-icon="season.action"
+        no-action
+        sub-group>
+        <template v-slot:activator>
+          <v-list-item-title>{{ phrases.journey }} {{ season.title }}</v-list-item-title>
+        </template>
+        <v-list-item
+          v-for="turtledove in season.turtledoves"
+          :key="turtledove.name">
+          <v-list-item-content>
+            <v-list-item-title>{{ turtledove.name }}</v-list-item-title>
+          </v-list-item-content>
+          <v-list-item-action>
+            <v-switch
+              v-model="turtledove.active"
+              inset />
+          </v-list-item-action>
+        </v-list-item>
+      </v-list-group>
+    </v-list-group>
+
+    <v-list-group
+      prepend-icon="mdi-twitter-circle"
+      no-action>
+      <template v-slot:activator>
+        <v-list-item-title>{{ phrases.who }}</v-list-item-title>
+      </template>
+      <v-list-item
+        v-for="(td, index) in turtledoves"
+        :key="index"
+        v-model="turtledoves"
+        inactive
+        ripple
+        @click="openLink(td.blog)">
+        <v-divider v-if="td.divider" />
+        <v-list-item-avatar v-if="td.avatar">
+          <v-img :src="td.avatar" />
+        </v-list-item-avatar>
+        <v-list-item-content v-if="td.name">
+          <v-list-item-title v-text="td.name" />
+        </v-list-item-content>
+      </v-list-item>
+    </v-list-group>
+
+    <v-list-group
+      prepend-icon="mdi-layers"
+      no-action>
+      <template v-slot:activator>
+        <v-list-item-title>{{ phrases.backgroundmap }}</v-list-item-title>
+      </template>
+      <v-list-item>
+        <v-radio-group
+          v-model="backgroundmap"
+          column>
+          <v-radio
+            :label="phrases.satellite"
+            value="satellite" />
+          <v-radio
+            :label="phrases.streetmap"
+            value="streetmap" />
+        </v-radio-group>
+      </v-list-item>
+    </v-list-group>
+
+    <v-list-group
+      prepend-icon="mdi-translate"
+      no-action>
+      <template v-slot:activator>
+        <v-list-item-title>{{ phrases.language }}</v-list-item-title>
+      </template>
+      <v-list-item>
+        <v-radio-group
+          v-model="language"
+          column>
+          <v-radio
+            :label="phrases.en"
+            value="en" />
+          <v-radio
+            :label="phrases.de"
+            value="de" />
+        </v-radio-group>
+      </v-list-item>
+    </v-list-group>
+  </v-list>
+</template>
+
+<script>
+import "@mdi/font/css/materialdesignicons.css";
+
+export default {
+  props: {
+    seasons: {type: Array, default(){return []}},
+    turtledoves: {type: Array, default(){return []}},
+    phrases: {type: Object, default(){return {}}},
+    backgroundmap: {type: String, default(){return "streetmap"}},
+    language: {type: String, default(){return ""}}
+  },
+  data: () => ({
+  }),
+  watch: {
+    backgroundmap: {
+      // function(newVal, oldVal)
+      handler: function(newVal) {
+        if (newVal === "streetmap") {
+          this.$emit("update:backgroundmap", "streetmap");
+        } else {
+          this.$emit("update:backgroundmap", "satellite");
+        }
+      },
+      immediate: false
+    },
+    // function(newVal, oldVal)
+    language: function(newVal) {
+      if (newVal === "de") {
+        this.$emit("update:language", "de");
+      } else {
+        this.$emit("update:language", "en");
+      }
+    },
+  },
+  methods: {
+    openLink(link) {
+      window.open(link, "_blank");
+    }
+  }
+};
+</script>
+
+<style scoped>
+.navi {
+  transform: none !important;
+  -webkit-transform: none !important;
+}
+</style>
