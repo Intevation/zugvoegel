45f392b0b83c03d6c79dd0f20b41bf74058eb6da
Bjoern Schilberg <bjoern.schilberg@intevation.de>
2020-05-21 11:15:32 +0200

    Set transpileDependencies, productionSourceMap, lintOnSave, devServer

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,11 @@ module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
   configureWebpack: {
     devtool: "source-map"
-  }
+  },
+  transpileDependencies: ["vuetify"],
+  productionSourceMap: false,
+  lintOnSave: "error",
+  devServer: {
+        disableHostCheck: true
+    }
 };
